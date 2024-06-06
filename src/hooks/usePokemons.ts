6656f6a18d5a95731171fb9892e56ab7f42dee8c
iwmartnz <iwmartnz@gmail.com>
2024-06-06 02:51:26 -0600

    Refactor

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { searchPokemons } from '../services/pokemons';
 import { Pokemon } from '../types';
 
@@ -10,21 +10,23 @@ export const usePokemons = ({ search }: HookProps) => {
     const [pokemon, setPokemon] = useState<null | Pokemon>(null);
     const [loading, setLoading] = useState(false);
     const [notFound, setNotFound] = useState(false);
+    const previousSearch = useRef(search);
 
-    const getPokemon = async (search: string) => {
+    const getPokemon = useCallback(async (search: string) => {
+        if (search === previousSearch.current) return;
         try {
             setLoading(true);
             setNotFound(false);
+            previousSearch.current = search;
             const newPokemon = await searchPokemons(search);
             setPokemon(newPokemon);
-            console.log(newPokemon);
         } catch (error) {
             setNotFound(true);
             throw new Error('Failed to get pokemon details');
         } finally {
             setLoading(false);
         }
-    };
+    }, []);
 
     return { getPokemon, pokemon, loading, notFound };
 };
